fc9698debb9c540678184f504abe42ca38c53ca1
aciacian <astri219@gmail.com>
2022-12-09 10:14:07 +0700

    final

---
+++ routes/employee.js
@@ -0,0 +1,214 @@
+var express = require('express');
+var router = express.Router();
+var Leave = require('../models/leave');
+var Attendance = require('../models/attendance');
+//var Project = require('../models/project');
+var moment = require('moment');
+var User = require('../models/user');
+var csrf = require('csurf');
+var csrfProtection = csrf();
+var moment = require('moment');
+const leave = require('../models/leave');
+
+router.use('/', isLoggedIn, function checkAuthentication(req, res, next) {
+    next();
+});
+
+
+
+router.get('/', function viewHome(req, res, next) {
+    res.render('Employee/employeeHome', {
+        title: 'Home',
+        userName: req.session.user.name,
+        csrfToken: req.csrfToken(),
+        moment: moment,
+        present: false
+    });
+});
+
+
+
+router.get('/apply-for-leave', function applyForLeave(req, res, next) {
+    res.render('Employee/applyForLeave', {
+        title: 'Pengajuan Lembur',
+        csrfToken: req.csrfToken(),
+        userName: req.session.user.name
+    });
+});
+
+
+
+router.get('/applied-leaves', function viewAppliedLeaves(req, res, next) {
+    var leaveChunks = [];
+
+    //find is asynchronous function
+    Leave.find({applicantID: req.user._id}).sort({_id: -1}).exec(function getLeaves(err, docs) {
+        var hasLeave = 0;
+        var durasirumus=0;
+        var a=0, b=0;
+        if (docs.length > 0) {
+            hasLeave = 1;
+        }
+        for (var i = 0; i < docs.length; i++) {
+            leaveChunks.push(docs[i]);
+            
+
+            console.log(durasirumus)
+        }
+
+        // for (var i = 0; i < leaveChunks.length; i++) {
+        //     leaveChunks[i].applicantID
+        // }
+
+        res.render('Employee/appliedLeaves', {
+            title: 'Daftar Lembur',
+            csrfToken: req.csrfToken(),
+            hasLeave: hasLeave,
+            leaves: leaveChunks,
+            durasi:durasirumus,
+            moment: moment,
+            userName: req.session.user.name
+        });
+    });
+
+});
+
+
+
+router.post('/view-attendance', function viewAttendanceSheet(req, res, next) {
+    var attendanceChunks = [];
+
+    Attendance.find({
+        employeeID: req.session.user._id,
+        month: new Date().getMonth() + 1,
+        year: new Date().getFullYear()
+    }).sort({_id: -1}).exec(function getAttendanceSheet(err, docs) {
+        var found = 0;
+        if (docs.length > 0) {
+            found = 1;
+        }
+        for (var i = 0; i < docs.length; i++) {
+            attendanceChunks.push(docs[i]);
+        }
+        res.render('Employee/viewAttendance', {
+            title: 'Attendance Sheet',
+            month: new Date().getMonth() + 1,
+            csrfToken: req.csrfToken(),
+            found: found,
+            attendance: attendanceChunks,
+            moment: moment,
+            userName: req.session.user.name
+        });
+    });
+
+
+});
+
+
+router.get('/view-attendance-current', function viewCurrentlyMarkedAttendance(req, res, next) {
+    var attendanceChunks = [];
+
+    Attendance.find({
+        employeeID: req.session.user._id,
+        month: new Date().getMonth() + 1,
+        year: new Date().getFullYear()
+    }).sort({_id: -1}).exec(function getAttendanceSheet(err, docs) {
+        var found = 0;
+        if (docs.length > 0) {
+            found = 1;
+        }
+        for (var i = 0; i < docs.length; i++) {
+            attendanceChunks.push(docs[i]);
+        }
+        res.render('Employee/viewAttendance', {
+            title: 'Attendance Sheet',
+            month: new Date().getMonth() + 1,
+            csrfToken: req.csrfToken(),
+            found: found,
+            attendance: attendanceChunks,
+            moment: moment,
+            userName: req.session.user.name
+        });
+    });
+});
+
+
+
+router.get('/view-profile', function viewProfile(req, res, next) {
+
+    User.findById(req.session.user._id, function getUser(err, user) {
+        if (err) {
+            console.log(err);
+
+        }
+        res.render('Employee/viewProfile', {
+            title: 'Profile',
+            csrfToken: req.csrfToken(),
+            employee: user,
+            moment: moment,
+            userName: req.session.user.name
+        });
+    });
+
+});
+
+router.post('/apply-for-leave', function applyForLeave(req, res, next) {
+
+    var newLeave = new Leave();
+    newLeave.applicantID = req.user._id;
+    newLeave.tujuanlembur = req.body.tujuanlembur;
+    newLeave.jammulailembur = req.body.jammulailembur;
+    newLeave.jamselesailembur = req.body.jamselesailembur;
+    newLeave.tipehari = req.body.tipehari;
+    newLeave.appliedDate = new Date();
+    newLeave.adminResponse = 'Waiting Approval';
+    newLeave.save(function saveLeave(err) {
+        if (err) {
+            console.log(err);
+        }
+        res.redirect('/employee/applied-leaves');
+    });
+
+});
+
+
+
+router.post('/mark-employee-attendance',async function markEmployeeAttendance(req, res, next) {
+    const attendance =  await Attendance.findOneAndUpdate({
+        employeeID: req.user._id,
+        month: new Date().getMonth()+ 1,
+        date: new Date().getDate(),
+        year: new Date().getFullYear(),
+    }, {waktupulang:req.body.waktupulang})
+
+    if(!attendance){
+
+            var newAttendance = new Attendance();
+            newAttendance.employeeID = req.user._id;
+            newAttendance.year = new Date().getFullYear();
+            newAttendance.month = new Date().getMonth() + 1;
+            newAttendance.date = new Date().getDate();
+            newAttendance.present = 1;
+            newAttendance.waktumasuk = req.body.waktumasuk
+            newAttendance.waktupulang = 0
+            newAttendance.save(function saveAttendance(err) {
+                if (err) {
+                    console.log(err);
+                }
+
+            });
+        }
+        res.redirect('/employee/view-attendance-current');
+
+    });
+module.exports = router;
+
+
+
+function isLoggedIn(req, res, next) {
+
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/');
+}
---
+++ routes/manager.js
@@ -0,0 +1,245 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+//var UserSalary = require('../models/user_salary');
+//var PaySlip = require('../models/payslip');
+var Leave = require('../models/leave');
+var Attendance = require('../models/attendance');
+var moment = require('moment');
+//var Project = require('../models/project');
+//var PerformanceAppraisal = require('../models/performance_appraisal');
+var flash = require('connect-flash');
+var csrf = require('csurf');
+var csrfProtection = csrf();
+
+router.use('/', isLoggedIn, function checkAuthentication(req, res, next) {
+    next();
+});
+
+
+router.get('/', function viewHomePage(req, res, next) {
+
+    res.render('Manager/managerHome', {
+        title: 'Manager Home',
+        csrfToken: req.csrfToken(),
+        userName: req.session.user.name
+    });
+});
+
+router.get('/view-employees', function viewAllEmployees(req, res, next) {
+
+    var userChunks = [];
+    var chunkSize = 3;
+    //find is asynchronous function
+    //department:req.session.user.department
+    User.find({$or: [{type: 'Pegawai',}]}).sort({_id: -1}).exec(function getUsers(err, docs) {
+        for (var i = 0; i < docs.length; i++) {
+            userChunks.push(docs[i]);
+        }
+        res.render('Manager/managerviewAllEmployee', {
+            title: 'All Employees',
+            csrfToken: req.csrfToken(),
+            users: userChunks,
+            userName: req.session.user.name
+        });
+    });
+
+
+});
+
+
+
+router.get('/leave-applications', function getLeaveApplications(req, res, next) {
+
+    var leaveChunks = [];
+    var employeeChunks = [];
+    var temp;
+    //find is asynchronous function
+    Leave.find({}).sort({}).exec(function findAllLeaves(err, docs) {
+        var hasLeave = 0;
+        if (docs.length > 0) {
+            hasLeave = 1;
+        }
+        for (var i = 0; i < docs.length; i++) {
+            leaveChunks.push(docs[i])
+        }
+        for (var i = 0; i < leaveChunks.length; i++) {
+
+            User.findById(leaveChunks[i].applicantID, function getUser(err, user) {
+                if (err) {
+                    console.log(err);
+                }
+                employeeChunks.push(user);
+
+            })
+        }
+
+        // call the rest of the code and have it execute after 3 seconds
+        setTimeout(render_view, 900);
+        function render_view() {
+            res.render('Manager/allApplications', {
+                title: 'Approval Lembur',
+                csrfToken: req.csrfToken(),
+                hasLeave: hasLeave,
+                leaves: leaveChunks,
+                employees: employeeChunks, moment: moment, userName: req.session.user.name
+            });
+        }
+    });
+
+});
+
+
+
+router.get('/respond-application/:leave_id/:employee_id', function respondApplication(req, res, next) {
+    var leaveID = req.params.leave_id;
+    var employeeID = req.params.employee_id;
+    Leave.findById(leaveID, function getLeave(err, leave) {
+
+        if (err) {
+            console.log(err);
+        }
+        User.findById(employeeID, function getUser(err, user) {
+            if (err) {
+                console.log(err);
+            }
+            res.render('Manager/applicationResponse', {
+                title: 'Approval Lembur',
+                csrfToken: req.csrfToken(),
+                leave: leave,
+                employee: user,
+                moment: moment, userName: req.session.user.name
+            });
+
+
+        })
+
+
+    });
+
+
+});
+
+
+
+router.get('/view-attendance-current', function viewCurrentMarkedAttendance(req, res, next) {
+
+    var attendanceChunks = [];
+
+    Attendance.find({
+        employeeID: req.user._id,
+        month: new Date().getMonth() + 1,
+        year: new Date().getFullYear()
+    }).sort({_id: -1}).exec(function getAttendanceSheet(err, docs) {
+        var found = 0;
+        if (docs.length > 0) {
+            found = 1;
+        }
+        for (var i = 0; i < docs.length; i++) {
+            attendanceChunks.push(docs[i]);
+        }
+        res.render('Manager/viewAttendance', {
+            title: 'Attendance Sheet',
+            month: new Date().getMonth() + 1,
+            csrfToken: req.csrfToken(),
+            found: found,
+            attendance: attendanceChunks,
+            moment: moment,
+            userName: req.session.user.name
+        });
+    });
+});
+
+router.get('/view-profile', function viewProfile(req, res, next) {
+
+    User.findById(req.user._id, function getUser(err, user) {
+        if (err) {
+            console.log(err);
+
+        }
+        res.render('Manager/viewManagerProfile', {
+            title: 'Profile',
+            csrfToken: req.csrfToken(),
+            employee: user,
+            moment: moment,
+            userName: req.session.user.name
+        });
+    });
+
+});
+
+router.post('/view-attendance', function viewAttendance(req, res, next) {
+
+    var attendanceChunks = [];
+    Attendance.find({
+        employeeID: req.user._id,
+        month: req.body.month,
+        year: req.body.year
+    }).sort({_id: -1}).exec(function getAttendanceSheet(err, docs) {
+        var found = 0;
+        if (docs.length > 0) {
+            found = 1;
+        }
+        for (var i = 0; i < docs.length; i++) {
+            attendanceChunks.push(docs[i]);
+        }
+        res.render('Manager/viewAttendance', {
+            title: 'Attendance Sheet',
+            month: req.body.month,
+            csrfToken: req.csrfToken(),
+            found: found,
+            attendance: attendanceChunks,
+            moment: moment,
+            userName: req.session.user.name
+        });
+    });
+
+
+});
+
+
+
+router.post('/mark-manager-attendance', function markAttendance(req, res, next) {
+
+    Attendance.find({
+        employeeID: req.user._id,
+        date: new Date().getDate(),
+        month: new Date().getMonth() + 1,
+        year: new Date().getFullYear()
+    }, function getAttendance(err, docs) {
+        var found = 0;
+        if (docs.length > 0) {
+            found = 1;
+        }
+        else {
+
+            var newAttendance = new Attendance();
+            newAttendance.employeeID = req.user._id;
+            newAttendance.year = new Date().getFullYear();
+            newAttendance.month = new Date().getMonth() + 1;
+            newAttendance.date = new Date().getDate();
+            newAttendance.present = 1;
+            newAttendance.save(function saveAttendance(err) {
+                if (err) {
+                    console.log(err);
+                }
+
+            });
+        }
+        res.redirect('/manager/view-attendance-current');
+
+    });
+
+
+});
+module.exports = router;
+
+
+
+function isLoggedIn(req, res, next) {
+
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/');
+}
